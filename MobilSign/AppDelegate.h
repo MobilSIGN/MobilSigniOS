8b1b2cf823240e82ecc6e15f3a30b6859417cc0c
MarekSpalek <marek.spalek@trashout.me>
2013-05-04 10:11:46 +0200

    Initial Commit

---
+++ MobilSign/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MobilSign
+//
+//  Created by Marek Špalek on 4. 5. 2013.
+//  Copyright (c) 2013 Marek Spalek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MobilSign/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MobilSign
+//
+//  Created by Marek Špalek on 4. 5. 2013.
+//  Copyright (c) 2013 Marek Spalek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
